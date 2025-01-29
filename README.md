466f2cdaf215894e33f77d0c77edfcf98b301f7e
Forrest Evans <fdevans@gmail.com>
2025-01-29 12:01:21 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Job Metrics View for Rundeck
---
+++ src/main/rdplugin/resources/js/init.js
@@ -0,0 +1,10 @@
+jQuery(function () {
+    if (typeof RDPLUGIN != "object") {
+      window.RDPLUGIN = {};
+    }
+    
+    // Initialize plugin
+    RDPLUGIN["ui-jobmetrics"] = {
+      name: "ui-jobmetrics"
+    };
+  });
